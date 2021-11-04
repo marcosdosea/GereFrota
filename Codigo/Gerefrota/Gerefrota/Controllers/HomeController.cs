6028491ec1ef32f467ecb3bd3f1ee7c498cc859a
Gabriel Santana <gabriel.sistemasjr@gmail.com>
2021-11-04 16:25:51 -0300

    Logica inicial da recuperação de usuario logado.

---
+++ Codigo/Gerefrota/Gerefrota/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Abstract.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Gerefrota.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IVeiculoService _veiculoService;
+        public HomeController(IVeiculoService veiculoService) => _veiculoService = veiculoService;
+
+        [HttpGet("{idFrota}")]
+        public IActionResult Get(int idFrota)
+        {
+            var identity = User.Identity as ClaimsIdentity;
+            var idUnidade = identity.Claims.Where(x => x.Type == ClaimTypes.PrimaryGroupSid).FirstOrDefault();
+            return Ok(_veiculoService.ObterTodosVeiculosDaFrota(idFrota));
+        }
+    }
+}
---
+++ Codigo/Gerefrota/Gerefrota/Extensions/Identity/GetIdentityUserExtension.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Models;
+using Domain.Models.Auxs;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Gerefrota.Extensions.Identity
+{
+    public static class GetIdentityUserExtension
+    {
+        public static UserAndTypeModel GetIdentityUser(this ClaimsIdentity userIdentity)
+        {
+            var claims = userIdentity.Claims;
+            var roleClaimType = userIdentity.RoleClaimType;
+
+            var userAndType = new UserAndTypeModel
+            {
+                Usuario = new UsuarioModel
+                {
+                    Id = int.Parse(claims.FirstOrDefault(c => c.Type == ClaimTypes.Sid).Value),
+                    Nome = claims.FirstOrDefault(c => c.Type == ClaimTypes.Name).Value,
+                    Email = claims.FirstOrDefault(c => c.Type == ClaimTypes.Email).Value,
+                    Telefone = claims.FirstOrDefault(c => c.Type == ClaimTypes.MobilePhone).Value,
+                    IdTipoUsuario = int.Parse(claims.FirstOrDefault(c => c.Type == ClaimTypes.Role).Value),
+
+                    new Claim(ClaimTypes.Sid, user.Usuario.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.Usuario.Nome),
+                    new Claim(ClaimTypes.Email, user.Usuario.Email),
+                    new Claim(ClaimTypes.MobilePhone, user.Usuario.Telefone),
+                    new Claim(ClaimTypes.Role, user.TipoUsuario.Descricao),
+                    new Claim(ClaimTypes.SerialNumber, user.Usuario.NumeroMatricula),
+                    new Claim(ClaimTypes.PrimaryGroupSid, user.Usuario.IdUnidade.ToString())
+                },
+                TipoUsuario = new TipoUsuarioModel { }
+            };
+
+            var roles = claims.FirstOrDefault(c => c.Type == ClaimTypes.PrimaryGroupSid);
+
+            return null;
+        }
+    }
+}
