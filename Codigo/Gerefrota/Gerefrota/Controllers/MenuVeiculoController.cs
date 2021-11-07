17b624744c8c71b0ed48ffd06552215a11ee3e53
Gabriel Santana <gabriel.sistemasjr@gmail.com>
2021-11-06 22:03:22 -0300

    Update MenuVeiculoController.cs

--- Codigo/Gerefrota/Gerefrota/Controllers/MenuVeiculoController.cs
+++ Codigo/Gerefrota/Gerefrota/Controllers/MenuVeiculoController.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Abstract.Services;
+using Domain.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,9 +11,27 @@ namespace Gerefrota.Controllerda
     public class MenuVeiculoController : ControllerBase
     {
         private readonly IVistoriaService _vistoriaService;
-        public MenuVeiculoController(IVistoriaService vistoriaService) => (_vistoriaService) = (vistoriaService);
+        private readonly IVeiculoService _veiculoService;
+        private readonly IAbastecimentoService _abastecimentoService;
+        private readonly IViagemService _viagemService;
+        public MenuVeiculoController(IVistoriaService vistoriaService, IVeiculoService veiculoService, IAbastecimentoService abastecimentoService, IViagemService viagemService)
+        {
+            _vistoriaService = vistoriaService;
+            _veiculoService = veiculoService;
+            _abastecimentoService = abastecimentoService;
+            _viagemService = viagemService;
+        }
 
         [HttpGet("{idVeiculo}")]
-        public IActionResult Get(int idVeiculo) => Ok(idVeiculo);
+        public IActionResult Get(int idVeiculo) => Ok(_veiculoService.Get(x => x.Id == idVeiculo));
+
+        [HttpPost("realizarVistoria")]
+        public IActionResult PostVistoria([FromBody] VistoriaModel vistoria) => Ok(_vistoriaService.Insert(vistoria));
+
+        [HttpPost("realizarAbastecimento")]
+        public IActionResult PostAbastecimento([FromBody] AbastecimentoModel abastecimento) => Ok(_abastecimentoService.Insert(abastecimento));
+
+        [HttpPost("registrarSaida")]
+        public IActionResult PostSaida([FromBody] ViagemModel viagem) => Ok(_viagemService.Insert(viagem));
     }
 }
